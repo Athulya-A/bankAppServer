e411a19998d0cb30ae7d73324e08a62927affd9c
Athulya <athuniran@gmail.com>
2022-03-17 11:37:12 +0530

    bank server

---
+++ services/data.service.js
@@ -0,0 +1,115 @@
+database = {
+  1000: { acno: 1000, uname: "Neer", password: 1000, balance: 5000, transaction: [] },
+  1001: { acno: 1001, uname: "Vyom", password: 1001, balance: 8000, transaction: [] },
+  1002: { acno: 1002, uname: "Laisha", password: 1002, balance: 15000, transaction: [] }
+}
+
+//   register definition
+
+const register = (acno, password, uname) => {
+
+  if (acno in database) {
+    return {
+      statusCode: 422,
+      status: false,
+      message: "User Already Exits....... Please Log In"
+    }
+  }
+  else {
+
+    database[acno] = {
+      acno,
+      uname,
+      password,
+      balance: 0,
+      transaction: []
+    }
+    console.log(database)
+
+    return {
+      statusCode: 200,
+      status: true,
+      message: "Successfully Registered!!!!"
+    }
+  }
+}
+
+
+//login definition
+
+const login = (acno, password) => {
+
+  if (acno in database) {
+    if (password == database[acno]["password"]) {
+      currentAcno = acno
+      currentUname = database[acno]["uname"]
+      return {
+        statusCode: 200,
+        status: true,
+        message: "Successfully log In!!!!",
+        currentAcno,
+        currentUname
+      }
+    }
+    else {
+      return {
+        statusCode: 422,
+        status: false,
+        message: "Incorrect password"
+      }
+    }
+  }
+  else {
+    return {
+      statusCode: 422,
+      status: false,
+      message: "User doesnot exist!!!!"
+    }
+  }
+}
+
+
+ //deposit definition
+
+ const deposit = (acno,password,amt)=>{
+  var amount = parseInt(amt)
+  if (acno in database) {
+
+    if (password == database[acno]["password"]) {
+
+      database[acno]["balance"] += amount
+      
+      database[acno]["transaction"].push({
+        amount: amount,
+        type: "CREDIT"
+      })
+      //console.log(database)
+      return {
+        statusCode: 200,
+        status: true,
+        message: amount+"Successfully deposited..And new balance is"+ database[acno]["balance"]
+
+      }
+    }
+    else {
+      return {
+        statusCode: 422,
+        status: false,
+        message: "Incorrect password"
+      }
+    }
+  }
+  else {
+    return {
+      statusCode: 422,
+      status: false,
+      message: "User doesnot exist!!!!"
+    }
+  }
+}
+
+module.exports = {
+  register,
+  login,
+  deposit
+}
